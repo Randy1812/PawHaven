220a8b497466971409390fc8404c83f1526d36df
Aryan Vats <avats101@gmail.com>
2022-04-22 21:01:25 +0530

    changed

--- model.py
+++ model.py
@@ -5,18 +5,7 @@ from nltk.sentiment import SentimentIntensityAnalyzer
 def sentiment_scores(sentence):
 	sid_obj = SentimentIntensityAnalyzer()
 	sentiment_dict = sid_obj.polarity_scores(sentence)
-	print("Overall sentiment dictionary is : ", sentiment_dict)
-	print("sentence was rated as ", sentiment_dict['neg']*100, "% Negative")
-	print("sentence was rated as ", sentiment_dict['neu']*100, "% Neutral")
-	print("sentence was rated as ", sentiment_dict['pos']*100, "% Positive")
-	print("Sentence Overall Rated As", end = " ")
-	if sentiment_dict['compound'] >= 0.05 :
-		print("Positive")
-	elif sentiment_dict['compound'] <= - 0.05 :
-		print("Negative")
-	else :
-		print("Neutral")
-
+	return sentiment_dict['pos']*100
 sentence = "This organisation was awesome. I am so happy with the service."
-sentiment_scores(sentence)
+print(sentiment_scores(sentence))
 
